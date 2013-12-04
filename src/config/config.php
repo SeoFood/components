3d38ff73d899ff9e2ed89c56d6d48aee0f2eea7c
Florian Uhlrich <f.uhlrich@fishingpool.de>
2013-12-04 10:29:50 +0100

    Comment config.php

--- src/config/config.php
+++ src/config/config.php
@@ -2,9 +2,20 @@
 
 return array(
 
+
+	/**
+	 * The Location where your Components are present, without Component Folder Name
+	 */
 	'location' => './',
+
+	/**
+	 * The component folder name
+	 */
 	'folderName' => 'components',
 
+	/**
+	 * Here you can define files, that autoload in each Module
+	 */
 	'file_autoload' => array(
 		'routes.php',
 		'filters.php'
